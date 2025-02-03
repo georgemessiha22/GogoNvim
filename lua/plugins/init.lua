a526f4c964486bd982b0c4d0905942d0ab72bece
George Messiha <georgemessiha22@gmail.com>
2025-02-03 16:06:17 +0400

    feat: adding avante.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,36 +1,37 @@
 return {
-	{ "folke/lazy.nvim",      version = "*" },
-	{ "nvim-lua/plenary.nvim" },
-	-- colors{{{
-	require("plugins.editor.colorscheme"),
-	-- }}}
+    { "folke/lazy.nvim",      version = "*" },
+    { "nvim-lua/plenary.nvim" },
+    -- colors{{{
+    require("plugins.editor.colorscheme"),
+    -- }}}
 
-	-- Code {{{
-	require("plugins.code.luasnip"),
-	require("plugins.code.blink"),
-	require("plugins.code.todo-comment"),
-	-- }}}
+    -- Code {{{
+    require("plugins.code.luasnip"),
+    require("plugins.code.blink"),
+    require("plugins.code.todo-comment"),
+    -- }}}
 
-	-- LSP {{{
-	require("plugins.lsp"),
-	-- }}}
+    -- LSP {{{
+    require("plugins.lsp"),
+    -- }}}
 
-	-- Git {{{
-	require("plugins.code.gitsigns"),
-	-- }}}
+    -- Git {{{
+    require("plugins.code.gitsigns"),
+    -- }}}
 
-	-- Editor View {{{
-	require("plugins.editor.snacks"),
-	require("plugins.editor.mininvim"),
-	require("plugins.editor.flash"),
-	require("plugins.editor.trouble"),
-	-- require("plugins.editor.harpoon2"),
-	require("plugins.editor.telescope"),
-	require("plugins.editor.dressing"),
-	{ "MunifTanjim/nui.nvim", lazy = true },
-	require("plugins.editor.noice"),
-	require("plugins.editor.whichkey"),
-  require("plugins.editor.persistence"),
-	-- require("plugins.editor.dashboard"),
-	-- }}}
+    -- Editor View {{{
+    require("plugins.editor.snacks"),
+    require("plugins.editor.mininvim"),
+    require("plugins.editor.flash"),
+    require("plugins.editor.trouble"),
+    -- require("plugins.editor.harpoon2"),
+    require("plugins.editor.telescope"),
+    require("plugins.editor.dressing"),
+    { "MunifTanjim/nui.nvim", lazy = true },
+    require("plugins.editor.noice"),
+    require("plugins.editor.whichkey"),
+    require("plugins.editor.persistence"),
+    -- require("plugins.editor.dashboard"),
+    require("plugins.editor.avante"),
+    -- }}}
 }
