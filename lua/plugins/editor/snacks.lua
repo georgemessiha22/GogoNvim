844f91524c5c4b7a90b6355a6209c3fba56711ed
George Messiha <georgemessiha22@gmail.com>
2025-07-08 16:21:42 +0400

    fix: remove snacks indent

--- lua/plugins/editor/snacks.lua
+++ lua/plugins/editor/snacks.lua
@@ -1,32 +1,32 @@
 return {
-	"folke/snacks.nvim",
-	priority = 1000,
-	lazy = false,
-	config = function()
-		require("snacks").setup(
-			{
-				picker = {
-					enabled = false,
-				},
-				dashboard = {
-					enabled = true,
-					preset = {
-						header = GogoUI.dash.header,
-					},
-				},
-				dim = { enabled = true },
-				indent = { enabled = true },
-				input = { enabled = true },
-				bigfile = { enabled = true },
-				notify = { enabled = true },
-				notifier = { enabled = true },
-				quickfile = { enabled = true },
-				scope = { enabled = true },
-				scratch = { enabled = true },
-				statuscolumn = { enabled = true },
-				words = { enabled = true },
-				lazygit = { enabled = true },
-			})
-		GogoVIM.load_mapping("snacks")
-	end
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    config = function()
+        require("snacks").setup(
+            {
+                picker = {
+                    enabled = false,
+                },
+                dashboard = {
+                    enabled = true,
+                    preset = {
+                        header = GogoUI.dash.header,
+                    },
+                },
+                dim = { enabled = true },
+                -- indent = { enabled = true },
+                input = { enabled = true },
+                bigfile = { enabled = true },
+                notify = { enabled = true },
+                notifier = { enabled = true },
+                quickfile = { enabled = true },
+                scope = { enabled = true },
+                scratch = { enabled = true },
+                statuscolumn = { enabled = true },
+                words = { enabled = true },
+                lazygit = { enabled = true },
+            })
+        GogoVIM.load_mapping("snacks")
+    end
 }
